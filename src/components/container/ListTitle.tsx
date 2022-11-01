6dbdbc5b84a32b38e652c331adb1640e7b05f185
phuocanh <docongphuocanh.2697@gmail.com>
2022-11-01 09:25:40 +0700

    edit style list titile

--- src/components/container/ListTitle.tsx
+++ src/components/container/ListTitle.tsx
@@ -1,5 +1,6 @@
 import {useTheme} from '@react-navigation/native';
 import {
+  Card,
   ChevronRightIcon,
   HStack,
   Image,
@@ -53,7 +54,7 @@ export default ({
   }
 
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={onPress}>
+    <TouchableOpacity activeOpacity={0.6} disabled={!onPress} onPress={onPress}>
       <HStack
         backgroundColor={colors.card}
         borderRadius="lg"
@@ -61,21 +62,17 @@ export default ({
         alignItems="center"
         marginTop={4}
         space={4}
+        // flex={1}
         {...props}>
         {leftComponent ?? (
-          <Image
-            size={16}
-            borderRadius="lg"
-            source={source}
-            alt={`listTitle-${title}`}
-          />
+          <Image size={16} borderRadius="lg" source={source} alt={`${title}`} />
         )}
 
-        <VStack justifyContent={'center'} space={2}>
+        <VStack justifyContent={'center'} space={2} flex={1}>
           <Text fontWeight="bold">{title}</Text>
-          {!!subTitle ?? <Text color={'gray.400'}>{subTitle}</Text>}
+          {!!subTitle && <Text color={'gray.400'}>{subTitle}</Text>}
         </VStack>
-        <Spacer />
+        {/* <Spacer /> */}
         {rightComponent ? rightComponent : <ChevronRightIcon />}
       </HStack>
     </TouchableOpacity>
